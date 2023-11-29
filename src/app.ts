3311cd1b22d0c27c6738245e0c2d4e0ed4fbbc78
gabriel-victor933 <gabrielvictor933@usp.br>
2023-11-29 17:21:49 -0300

    feat(api): Implement middleware for validating request bodies using schemas

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import participantsRouter from "./routers/participantsRouter"
 import betsRouter from "./routers/betsRouter"
 
 const app = express()
+app.use(express.json())
 
 app.get("/health",(req: Request, res: Response) => res.status(httpStatus.OK).send("ok"));
 app.use("/games",gamesRouter)
--- src/routers/participantsRouter.ts
+++ src/routers/participantsRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-
+import {validateSchemaMiddleware} from "@/middlewares/validateSchemaMiddleware";
+import participantsSchema from "@/schemas/participantsSchema";
 const route = Router()
 
 route.get("/",(req,res)=>res.send("oka"))
-route.post("/",(req,res)=>res.send("oka"))
+route.post("/",validateSchemaMiddleware(participantsSchema),(req,res)=>res.send("oka12"))
 
 export default route
