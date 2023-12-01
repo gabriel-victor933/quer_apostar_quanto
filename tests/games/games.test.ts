c03978d3c0bd94ebc901059e804c662832623eb0
gabriel-victor933 <gabrielvictor933@usp.br>
2023-12-01 15:54:49 -0300

    test(integration): Add integration tests for GET /games by ID endpoint

--- tests/games/games.test.ts
+++ tests/games/games.test.ts
@@ -68,3 +68,38 @@ describe("route GET /games", () => {
         expect(result.body).toHaveLength(0)
     })
 })
+
+describe("route GET /games by id",()=>{
+
+    it("should return an specific game", async () => {
+        const game = await insertGameInDb()
+        const result = await api.get(`/games/${game.id}`)
+
+        expect(result.statusCode).toBe(httpStatus.OK)
+        expect(result.body).toEqual(expect.objectContaining({
+            id: game.id,
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            homeTeamName: expect.any(String),
+            awayTeamName: expect.any(String),
+            homeTeamScore: expect.any(Number), 
+            awayTeamScore: expect.any(Number), 
+            isFinished: expect.any(Boolean),
+        
+        }))
+    })
+
+    it("should return NOT FOUND if game doesn't exist", async () => {
+        const result = await api.get(`/games/${Math.round(Math.random()*10)}`)
+
+        expect(result.statusCode).toBe(httpStatus.NOT_FOUND)
+
+    })
+
+    it("should return BAD REQUEST if id is not a number", async () => {
+        const result = await api.get(`/games/a`)
+
+        expect(result.statusCode).toBe(httpStatus.BAD_REQUEST)
+
+    })
+})
